97b8136ceacc6a6d7a7c9e35cb76c0472672ca96
YairAndrade1 <kidyarson@gmail.com>
2025-09-12 10:35:44 -0500

    feat(add): Navbar and basic form

---
+++ src/app/navbar.tsx
@@ -0,0 +1,16 @@
+
+export default function navBar() {
+    return (
+        <nav>
+            <div className="flex items-center h-20 bg-white text-gray-800">
+                <div className="mx-4 p-6"><img src="/logo.png" alt="Logo" className="h-12" /></div>
+                <div className="flex-grow text-center">
+                    <a href="" className="mx-4 hover:border-b-2">UserName</a>
+                    <a href="" className="mx-4 hover:border-b-2">Phone Number</a>
+                    <a href="" className="mx-4 hover:border-b-2">Address</a>
+                    <a href="" className="mx-4 hover:border-b-2">Toggle Theme</a>
+                </div>
+            </div>
+        </nav>
+    );
+}
